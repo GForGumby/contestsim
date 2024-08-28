a7a96b7de3cb485cb241741ba50aad416b6077bd
Gumby <156488743+GForGumby@users.noreply.github.com>
2024-08-28 07:32:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,16 @@ import numpy as np
 from numba import jit, prange
 from st_paywall import add_auth
 
+st.title("GUMBY SIMS- The first publicly available contest sim tool on Underdog. Take your game to the next level by simming out the highest ROI lineups,  optimal exposure percentages, and chalkiest field combos. ")
+
 st.write("TO MANAGE YOUR SUBSCRIPTION GO HERE https://billing.stripe.com/p/login/9AQ4jFeRDbHTaIMfYY")
 st.write("Please watch the entirety of this video explaining the product before purchasing (https://www.youtube.com/watch?v=tF9BU7yNdDI). I am available on twitter @GumbyUD for any questions or concerns about the product.")
 st.write("Use promo code LAUNCH for just $1 for the first week!")
 st.write("On a CFB sized slate, it takes about 3 mins to run the draft sim (1884 sims), and about 5 mins to run 5000 instances of the projection sim.")
+st.write("For the team stacking bonus, use .99 if you want stack frequency to minic real drafts. Use .98 if you want slightly more, and 1.00 for no stacking. The lower it is the more frequent stacks are in your field of lineups. I wouldn't make it lower than .95 except for MLB, which I am currently still testing.") 
 
 
-st.title("GUMBY SIMS- The first publicly available contest sim tool on Underdog. Take your game to the next level by simming out the highest ROI lineups,  optimal exposure percentages, and chalkiest field combos. ")
+
 
 
 
